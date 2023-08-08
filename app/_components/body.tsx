282e5d5854ee4d4afcb77a861ff9e87ec3278e29
JakeSchroder <jake.schroder@gmail.com>
2023-08-08 05:01:38 -0500

    Create webpage components

---
+++ app/_components/body.tsx
@@ -0,0 +1,43 @@
+import CertificationShowcase from "./certification_showcase";
+import ProjectShowcase from "./project_showcase";
+
+const projects = [
+    {
+        name: 'Spotify Mixtape',
+        image_src:'/spotify-mixtape-site.png',
+        url: 'https://www.mixtape.digital',
+        github: 'https://github.com/JakeSchroder/spotify-mixtape',
+    },
+    {
+        name: 'Packrat',
+        image_src: '/packrat-site.png',
+        url: 'https://www.packrat.shop',
+        github: 'https://github.com/JakeSchroder/packrat-v2',
+    },
+    {
+        name: 'Portfolio-V1',
+        image_src: '/portfolio-v1-site.png',
+        url: '',
+        github: 'https://github.com/JakeSchroder/portfolio-jake'
+    }
+]
+export default function Body(){
+    return(
+        <div className=" flex flex-col ">
+            <div className=" flex flex-col items-center justify-center h-screen">
+                <h1 className=" text-8xl sm:text-3xl md:text-6xl text-center">Hello, I'm <b className="hover:text-gradient ">Jake Schroder,</b></h1>
+                <h1 className=" text-8xl sm:text-3xl md:text-6xl text-center font-bold hover:text-gradient">Fullstack Developer</h1>
+            </div>
+            {
+                projects.map((project, index) => <ProjectShowcase 
+                    name={project.name} 
+                    image_src={project.image_src} 
+                    url={project.url} 
+                    github={project.github}
+                />
+                )
+            }
+            <CertificationShowcase/>
+        </div>
+    )
+}
---
+++ app/_components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <div className=" flex items-center justify-center fixed gap-5 w-screen backdrop-blur-lg bg-white/50 h-24">
+            <a className=" btn-nav" href='https://github.com/JakeSchroder'>Github</a>
+            <a className=" btn-nav" href="https://www.linkedin.com/in/jakeschroder/">LinkedIn</a>
+        </div>
+    )
+}
