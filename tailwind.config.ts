0a46195143f85be24ca7ee8fc084b08ed8e992ce
JakeSchroder <jake.schroder@gmail.com>
2023-08-08 05:01:20 -0500

    Personalize tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors:{
+        text:'#000000',
+        background: '#FFFFFF',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
+      screens: {
+        sm: { max: '767px' },
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+
+        md: { min: '768px', max: '1023px' },
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+        lg: { min: '1024px', max: '1279px' },
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+        xl: { min: '1280px', max: '1535px' },
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+        '2xl': { min: '1536px' },
+        // => @media (min-width: 1536px) { ... }
       },
     },
   },
