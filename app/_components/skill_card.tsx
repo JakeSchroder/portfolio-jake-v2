adf2e30ca77259dc78903e8ea7dc7d77cbe08fa9
JakeSchroder <jake.schroder@gmail.com>
2023-08-16 18:07:06 -0500

    Fix left margin for skill icons on mobile

--- app/_components/skill_card.tsx
+++ app/_components/skill_card.tsx
@@ -9,7 +9,7 @@ export default function SkillCard(props:DefaultProps){
     return(
         <div className=" flex flex-row items-center justify-start w-[200px] h-[45px] bg-neutral-200 rounded-md gap-5 hover:shadow-lg hover:box-gradient sm:w-[45px] sm:justify-center">
             <Image
-                className=" ml-5 select-none sm:left-0"
+                className=" ml-5 select-none sm:ml-0"
                 src={props.image_src}
                 alt={`${props.name} icon`}
                 width={24}
