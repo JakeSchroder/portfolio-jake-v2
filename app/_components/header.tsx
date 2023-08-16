9d54d7a07067c7d748aefb3cd0cdfaa94c075634
JakeSchroder <jake.schroder@gmail.com>
2023-08-15 20:04:57 -0500

    Add resume

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,6 +3,7 @@ export default function Header(){
         <div className=" flex items-center justify-center fixed gap-5 w-screen backdrop-blur-lg bg-white/50 h-24">
             <a className=" btn-nav" href='https://github.com/JakeSchroder'>Github</a>
             <a className=" btn-nav" href="https://www.linkedin.com/in/jakeschroder/">LinkedIn</a>
+            <a className=" btn-nav" href="https://app.enhancv.com/share/2fb5e132/">Resume</a>
         </div>
     )
 }
